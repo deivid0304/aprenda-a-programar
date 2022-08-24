3f053a3cd5ae32db3c6d6df4610e34114b4561fa
Deivid Marcio <deivid_marcio3110@hotmail.com>
2022-08-24 11:21:23 -0300

    teste de variavel

--- Hello World.js
+++ Hello World.js
@@ -1,3 +1,25 @@
-console.log("Hello World");
-console.log("Óla mundo");
-console.log("Bonjou mond");
+//Comentário no código
+//Comentários não são executados pelo computador
+//Comentários so servem para serem lidos por nós humanos
+
+let variavel1 = "Texto aqui";
+//Na linha 5:
+//Variavel1 <- "Texto aqui"
+
+let variavel2 = 1;
+//Na linha 9:
+//variavel1 <- "Texto aqui"
+//variavel2 <- 1
+
+variavel1 = "Ituring";
+//Na linha 14:
+//variavel1 <- "Ituring"
+//variavel2 <- 1
+
+variavel1 = "Discord";
+//Na linha 19:
+//variavel1 <- "Discord"
+//variavel2 <- 1
+
+console.log(variavel1);
+console.log(variavel2);
