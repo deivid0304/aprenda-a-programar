8b562c1d66e98255d3873b5114219f74492518e0
Deivid Marcio <deivid_marcio3110@hotmail.com>
2022-08-25 16:40:45 -0300

    Tabuada de multiplicação

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+//Tabuada de multiplicação de 1 a 10.//
+
+let numeroBase = 9;
+let contador = 1;
+let multiplicador = 10;
+
+while(contador <= multiplicador){
+    console.log(`${numeroBase} x ${contador} = ${contador * numeroBase}`);
+    contador++;
+}
