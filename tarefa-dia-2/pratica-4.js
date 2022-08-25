f7ceeab3af1ec0ae589e62d8f3efb45e20e98e26
Deivid Marcio <deivid_marcio3110@hotmail.com>
2022-08-25 06:19:38 -0300

    Atualização do IF - ELSE

---
+++ tarefa-dia-2/pratica-4.js
@@ -0,0 +1,7 @@
+let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
+let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
+let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
+
+function taxaPorProduto(nome, ){
+
+}
